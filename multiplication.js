97624fe65420f650c61ac393acd87d49e7ef9e5b
natashamasimbe <natashamasimbe72@gmail.com>
2026-06-11 13:43:18 +0200

    Add multiplication programme

---
+++ multiplication.js
@@ -0,0 +1,15 @@
+// ========================================
+// MULTIPLICATION TABLE
+// ========================================
+
+let number = 5;
+
+console.log("==========================================");
+console.log("MULTIPLICATION TABLE FOR " + number);
+console.log("==========================================");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " x " + i + " = " + (number * i));
+}
+
+console.log("==========================================");
