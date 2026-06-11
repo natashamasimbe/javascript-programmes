40f9ae4a996382ad6145d4149caf34b8890f1e15
natashamasimbe <natashamasimbe72@gmail.com>
2026-06-11 13:40:21 +0200

    Add passorfail programme

---
+++ passorfail.js
@@ -0,0 +1,22 @@
+// ========================================
+// PASS OR FAIL
+// ========================================
+
+let studentName = "Natasha";
+let mark = 75;
+
+console.log("==========================================");
+console.log("STUDENT RESULT");
+console.log("==========================================");
+console.log("Student Name: " + studentName);
+console.log("Mark:         " + mark + "%");
+
+if (mark >= 50) {
+    console.log("Result:       PASS");
+    console.log("Comment:      Congratulations!");
+} else {
+    console.log("Result:       FAIL");
+    console.log("Comment:      Better luck next time!");
+}
+
+console.log("==========================================");
